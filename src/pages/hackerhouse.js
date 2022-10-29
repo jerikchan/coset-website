0906174b0a0ff5828ffb6a3356d405d32bf80170
qiuwan <973146586@qq.com>
2022-10-29 19:31:18 +0800

    add: hackerhouse page

---
+++ src/pages/hackerhouse.js
@@ -0,0 +1,10 @@
+import React from "react"
+// import { Link } from "gatsby"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    {/* <Link to="/">Home</Link> */}
+ 
+    <p>Send us a message!</p>
+  </div>
+)
