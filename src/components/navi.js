c2f90f0deeba637add4f62701b69f7f3c563ac8c
qiuwan <973146586@qq.com>
2023-01-05 00:41:09 +0800

    fix:pic

--- src/components/navi.js
+++ src/components/navi.js
@@ -1,5 +1,5 @@
 import React from "react"
-import logo from "../../content/images/hackerhouse/antalpha.png"
+import logo from "../../content/images/hackerhouse/antalpha.svg"
 import "./navi.css"
 export default (props) => (
   <div className="navi" style={{ ...props.styles, height: `80px` }}>
