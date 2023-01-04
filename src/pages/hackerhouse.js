626c0e24ed1aa4712320e3a3ef64a6a4dc04c4a6
qiuwan <973146586@qq.com>
2023-01-04 13:18:54 +0800

    add

--- src/pages/hackerhouse.js
+++ src/pages/hackerhouse.js
@@ -42,7 +42,7 @@ export default function HackerHouse() {
             belong: "UPCOMING"
         }, {
             id: '3',
-            signuplink: "",//跳转到notion项目页面
+            signuplink: "https://www.notion.so/antalpha/1a7771e12a444d1b96dd8cfc3f72ed4d?v=b373397745164802ad82571141dd5fcc",//跳转到notion项目页面
             theme: "# Infra and open source tools",
             "name": "",
             startDate: "21st Feb, 2023",
