8382b90b9d629d10ad2e343bc03e0423a3a945ab
qiuwan <973146586@qq.com>
2023-01-13 15:42:12 +0800

    addicon: mirror

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,11 @@
 import React from "react"
 import "./footer.css"
 import footerlogo from "../../content/images/footer/antalpha.svg"
-import notion from "../../content/images/footer/Notion.svg"
+import notion from "../../content/images/footer/notion.svg"
 import youtube from "../../content/images/footer/youtube.svg"
 import twitter from "../../content/images/footer/twitter.svg"
+import mirror from "../../content/images/footer/mirror.svg"
+
 export default () => (
   <div className="footer">
   <div className="footer-left">
@@ -13,6 +15,7 @@ export default () => (
           </div>
   </div>
   <div className="footer-right">
+      <a href="https://mirror.xyz/0xeA68d8B403FE1891ae4F309ABeE6D3C9D1089b25"> <img src={mirror} alt=" "></img></a>
       <a href="https://antalpha.notion.site/Antalpha-HackerHouse-13884d3f3d214ed59d430bb472ce523c"><img src={notion} alt=" "></img></a>
       <a href="https://www.youtube.com/channel/UCNFowsoGM9OI2NcEP2EFgrw"><img src={youtube} alt=" "></img></a>
       <a href="https://twitter.com/Antalpha_Labs"> <img src={twitter} alt=" "></img></a>
