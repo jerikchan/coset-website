a5c2d928d7c0fdda35b36fd59e2545b4b3f86e2f
qiuwan <973146586@qq.com>
2023-03-01 00:02:03 +0800

    uodate

--- src/pages/hackerhouse/chiangmai.js
+++ src/pages/hackerhouse/chiangmai.js
@@ -3,8 +3,8 @@ import Navi from "../../components/navi"
 import "../global.css";
 import "./chiangmai.css";
 import hh_chiangmai  from "../../../content/images/hackerhouse/chiangmai/hh_chiangmai.png";
-import bgleft from "../../../content/images/hackerhouse/chiangmai/bg-left.png";
-import bgright from "../../../content/images/hackerhouse/chiangmai/bg-right.png";
+import left from "../../../content/images/hackerhouse/chiangmai/left1.png";
+import right from "../../../content/images/hackerhouse/chiangmai/right2.png";
 
 import {Seo} from "gatsby-theme-portfolio-minimal";
 
@@ -19,17 +19,17 @@ export default function chiangmai() {
 
             {/* 2、报名页面 */}
             <div className="chiangmai">
-            <div className="chiangmai_bgleft ">
-                <img src={bgleft}></img>
+            <div className="chiangmai_bgleft">
+                {/* <img src={left}></img> */}
             </div>
                 <div className="chiangmai_activity">
 
-                  <img src={hh_chiangmai} className="chiangmai_activity_pic"></img>              
+                  {/* <img src={hh_chiangmai} className="chiangmai_activity_pic"></img>               */}
                     <div className="chiangmai_timelink">
                     <span className="chiangmai_time">08th Apr 2023 - 29th Apr 2023</span>
 
                     <a href="https://www.notion.so/antalpha/AHH-Thailand-ZKP-April-2023-6a51c13fdd654a6792d536a4460735ec">
-                        <button className="chiangmai_button" onclick="changeColor('green')">Coming soon</button>
+                        <button className="chiangmai_button" onclick="changeColor('green')">Apply to Hack</button>
                     </a>
                     <div className="chiangmai_contact">
                         <span className="chiangmai_add">Interesting in partnering?</span>
@@ -40,7 +40,7 @@ export default function chiangmai() {
                     </div>
                 </div>
                 <div className="chiangmai_bgright">
-                    <img src={bgright}></img>
+                    {/* <img src={right}></img> */}
                 </div>
             </div>
         </>
