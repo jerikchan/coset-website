46dc4070334cea5374d9f5e27cf502f65c473848
AmagiDDmxh <amagiddmxh@gmail.com>
2023-08-04 01:13:28 +0800

    update sui hackerhouse info (#13)

--- src/pages/hackerhouse.jsx
+++ src/pages/hackerhouse.jsx
@@ -43,9 +43,9 @@ export default function HackerHouse() {
     {
       signuplink: "/hackerhouse/sui",
       theme: "# Move",
-      name: "x Sui",
-      startDate: "3rd August, 2023",
-      endDate: "24th August, 2023",
+      name: "x Sui Move",
+      startDate: "3rd Sep, 2023",
+      endDate: "24th Sep, 2023",
       location: "@Dali",
     },
 
