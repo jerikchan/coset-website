ed856d4e854945a37a191fc66daf2a2fe5ba73c7
AmagiDDmxh <amagiddmxh@gmail.com>
2023-09-07 19:55:30 +0800

    fix ssr error (#16)

--- src/pages/hackerhouse/istanbul.tsx
+++ src/pages/hackerhouse/istanbul.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react"
 
 const NOTION_PAGE = 'https://www.notion.so/antalpha/Qiming-AHH-ZKonnect-Istanbul-Oct-22nd-Nov-20th-2023-Event-b2d33bb40ab34a67a004de5f2f9bfa42'
 
 export default function Istanbul() {
-  return location.replace(NOTION_PAGE)
+  useEffect(() => {
+    if (self.location) {
+      self.location.replace(NOTION_PAGE)
+    }
+  }, [])
+  
+  return null
 }
