3842893886c37677c9c6cc87a00db7e7302da380
AmagiDDmxh <amagiddmxh@gmail.com>
2023-09-07 15:16:22 +0800

    redirect istanbul to notion page (#15)

---
+++ src/pages/hackerhouse/istanbul.ts
@@ -0,0 +1,6 @@
+
+const NOTION_PAGE = 'https://www.notion.so/antalpha/Qiming-AHH-ZKonnect-Istanbul-Oct-22nd-Nov-20th-2023-Event-b2d33bb40ab34a67a004de5f2f9bfa42'
+
+export default function Istanbul() {
+  return location.replace(NOTION_PAGE)
+}
